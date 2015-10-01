846ad05d9c137a50c2d4cd7e84bff13a0c8a2675
elifoster <elifosterwy@gmail.com>
2015-10-01 16:55:51 -0700

    Documentation.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # String-Utility-Ruby
 Provides some basic String utilities for Ruby
+
+## Installation
+```
+gem install string-utility
+```
+
+## Documentation
+Please see the [Wiki](https://github.com/elifoster/string-utility-ruby/wiki) for feature lists, documentation, and guides.
+
+You can also view the [RubyDocs](http://www.rubydoc.info/gems/string-utility) but they are more limited.
+
+You can also view the documentation in the code, which would probably be fastest for you anyway.
--- lib/utils.rb
+++ lib/utils.rb
@@ -4,7 +4,7 @@ class StringUtility
   def initialize; end
 
   # Separates the string by another string. Useful for converting integers into human-readable numbers.
-  # @param string [String] The string to separate
+  # @param string [String] The string to separate.
   # @param count [Int] The number of integers to separate by. Defaults to 3.
   # @param separator [String] The string to use as a separator. Can only be 1 character long. Will use the first character if it is greater than 1 character long. Defaults to a comma.
   # @return [String] Formatted version of the provided string.
